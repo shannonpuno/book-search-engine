1e6b7ab798010e1d640ddf642413576564165f4a
Shannon <shannonpuno@arizona.edu>
2023-03-08 16:12:22 -0700

    fixing heroku deploy

--- server/server.js
+++ server/server.js
@@ -28,9 +28,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 
 // Apollo server w/ GraphQL Schema
